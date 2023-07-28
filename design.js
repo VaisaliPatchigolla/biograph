76ecf4774f22199fb804d5a4fcab8cc52ec2367d
Patchigolla H N SSS Vaisali <125901139+VaisaliPatchigolla@users.noreply.github.com>
2023-07-28 21:43:31 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,16 @@
+ReactDOM.render(
+    <div>
+    <h1>Sudha Murthy</h1>
+<img src="https://jaipurliteraturefestival.org/uploads_jaipurlitfest/speakers/1365-sudha-murty.jpg" width='250' height='250'/>
+<p>Sudha Murthy is also the chairperson and trustee of Infosys Foundation. With her master's in Electrical Engineering from the Indian Institute of Science, Bangalore, she started Infosys Foundation in 1996. She has built 2300 houses in flood-affected areas through the foundation.</p>
+ <uL>
+<li>Born       :19 August 1950 (age 72),Shiggaon, Bombay State (present-day Karnataka), India</li>
+<li>Alma mater :B.V.B. College of Engineering and Technology,Indian Institute of Science,Cornell University, Ithaca, New York</li>
+<li>Occupation :Chairperson of Infosys Foundation,Children's writer</li>
+<li>Books      :Notable work,	How I Taught My Grandmother to Read,Mahashweta,Dollar Bahu</li>
+<li>Spouse	   :N. R. Narayana Murthy</li>
+<li>Children   :Akshata Murty and Rohan Murty</li>
+<li>Awards	   :Padma Shri,Daana Chintamani Attimabbe Award,Padma Bhushan</li>
+</uL>
+    </div>,
+    document.getElementById('content'));
